2388edfae13e23ae3c43c8fccc793ea05cb5fe7e
ammarAlzaqam <git@ammaralzaqam.com>
2025-05-24 16:14:28 +0300

    EvenOrOdd 2functions

--- even-ood.js
+++ even-ood.js
@@ -1,1 +1,9 @@
 //TODO>> even odd numbers
+function evenOdd(value) {
+  if (value % 2 === 0) {
+    return "Even";
+  }
+  return "Odd";
+}
+
+const evenOrOdd = (number) => (number % 2 === 0 ? "Even" : "Odd");
