5cda001c43e842f01759ee54e72983c1319e9403
ammarAlzaqam <git@ammaralzaqam.com>
2025-06-24 15:55:53 +0300

    repeat-string

---
+++ repeate-str/index.js
@@ -0,0 +1,12 @@
+// TODO >> Create a function that repeats a given string `s`, `n` number of times and returns the final result.
+function repeatString(s, n) {
+//   let repeatedStr = "";
+//   for (let i = 0; i < n; i++) {
+//     repeatedStr += s;
+//   }
+//   return repeatedStr;
+
+  return s.repeat(n);
+}
+
+console.log(repeatString("ammar ", 3)); // ➜ "ammar ammar ammar"
