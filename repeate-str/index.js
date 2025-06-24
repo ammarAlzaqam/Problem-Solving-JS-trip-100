f4cfb112ffe7efcde09f28d2cfd13dd8a86fe1b0
ammarAlzaqam <git@ammaralzaqam.com>
2025-06-24 15:58:19 +0300

    repeat string sort

--- repeate-str/index.js
+++ repeate-str/index.js
@@ -1,11 +1,13 @@
 // TODO >> Create a function that repeats a given string `s`, `n` number of times and returns the final result.
 function repeatString(s, n) {
-//   let repeatedStr = "";
-//   for (let i = 0; i < n; i++) {
-//     repeatedStr += s;
-//   }
-//   return repeatedStr;
+  //! with loop
+  //* let repeatedStr = "";
+  //* for (let i = 0; i < n; i++) {
+  //*   repeatedStr += s;
+  //* }
+  //* return repeatedStr;
 
+  //! using build in function
   return s.repeat(n);
 }
 
